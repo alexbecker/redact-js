bde68646c32849c05a5d232be8bc76b5e045ebe6
Alex Becker <alex.becker@drchrono.com>
2016-03-11 09:37:00 -0800

    WIP: use IVs and use password hashes to unlock lower-clearance passwords

--- redact.js
+++ redact.js
@@ -2,22 +2,19 @@
 
 var fs = require("fs");
 var aes = require("crypto-js/aes");
+var sha256 = require("crypto-js/sha256");
 var jsdom = require("jsdom");
 
-var argv = require("minimist")(process.argv.slice(2));
-var infile = argv._[0];
-var passwords = argv._.slice(1);
-var client_side_aes = argv.aes || "https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js";
-
-function redact(node, passwords) {
+function redact(node, passwords, iv) {
     var childToRedact = node.querySelector("span.redact");
     if (childToRedact !== null) {
-        redact(childToRedact, passwords.slice(1));
+        redact(childToRedact, passwords.slice(1), iv);
 
-        childToRedact.innerHTML = aes.encrypt(childToRedact.innerHTML, passwords[0]);
+        childToRedact.setAttribute("data-iv", iv.toString(16));
+        childToRedact.innerHTML = aes.encrypt(childToRedact.innerHTML, passwords[0], {'iv': iv});
         childToRedact.className = childToRedact.className.replace(/(?:^|\s)redact(?!\S)/g, " redacted ");
 
-        redact(node, passwords);
+        redact(node, passwords, iv + 1);
     }
 }
 
@@ -37,11 +34,19 @@ function injectDependencies(doc) {
     head.appendChild(decrypt);
 }
 
+var argv = require("minimist")(process.argv.slice(2));
+var infile = argv._[0];
+var passwords = argv._.slice(1);
+var client_side_aes = argv.aes || "https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js";
+
 var raw = fs.readFileSync(infile).toString();
+var hash = sha256(raw).toString();
+var iv = parseInt(hash.substring(0,8), 16);
+
 jsdom.env(
     raw,
     function (err, window) {
-        redact(window.document, passwords);
+        redact(window.document, passwords, iv);
         injectDependencies(window.document);
         process.stdout.write(window.document.documentElement.outerHTML);
     }
--- unredact.js
+++ unredact.js
@@ -2,7 +2,8 @@ function unredact(password) {
     var redactedNodes = document.querySelectorAll("span.redacted");
     for (var i=0; i<redactedNodes.length; i++) {
         var redacted = redactedNodes[i];
-        redacted.innerHTML = CryptoJS.AES.decrypt(redacted.innerHTML, password).toString(CryptoJS.enc.Utf8);
+        var iv = CryptoJS.enc.Hex.parse(redacted.getAttribute("data-iv"));
+        redacted.innerHTML = CryptoJS.AES.decrypt(redacted.innerHTML, password, {'iv': iv}).toString(CryptoJS.enc.Utf8);
         redacted.className = redacted.className.replace(/(?:^|\s)redacted(?!\S)/g, " redact ");
     }
 }
